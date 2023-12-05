44bff9f4ac43fd617b4c039e995892e9aab3e2c5
Tommy <tommaso.merli97@gmail.com>
2023-12-05 12:56:41 +0100

    added component support

---
+++ session/public/js/components/Profile.mjs
@@ -0,0 +1,36 @@
+import { internalMemory } from "../utilities/memory.mjs";
+
+export const Profile = (props = { root, onChangeUI }) => {
+    const auth = internalMemory.get("auth");
+    const state = {
+        user: auth?.user || null,
+        token: auth?.token || null,
+    }
+
+    const handleLogout = () => {
+        internalMemory.remove("auth");
+
+        document.removeEventListener("click", handleLogout);
+        
+        props.onChangeUI("/login");
+    }
+
+    document.addEventListener("click", handleLogout);
+
+    props.root.innerHTML = `
+    <div class="w-screen h-screen flex justify-center items-center flex-col" id="profile-page">
+        <div class="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 dark:bg-gray-900 dark:text-gray-100">
+            <img src="https://source.unsplash.com/150x150/?portrait?3" alt="" class="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square">
+            <div class="space-y-4 text-center divide-y dark:divide-gray-700">
+                <div class="my-2 space-y-1">
+                    <h2 id="full_name" class="text-xl font-semibold sm:text-2xl">${state.user.first_name} ${state.user.last_name}</h2>
+                    <p id="user_email" class="px-5 text-xs sm:text-base dark:text-gray-400">${state.user.email}</p>
+                </div>
+                <div class="flex justify-center pt-2 space-x-4 align-center">
+                    <button id="logout" class="border py-1 px-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400">Logout</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+}
