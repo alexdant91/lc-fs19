898fa3d96626aecd8385e23471ebb195fb3b7398
Tommy <tommaso.merli97@gmail.com>
2024-02-13 11:14:41 +0100

    correc syntax of readData, writeData

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const app = express.Router();
 
-const fs = require("fs");
-const path = require("path");
-
-const readData = (label = "users") => {
-    return label != null ? JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/data.json"), { encoding: 'utf8', flag: 'r' }))[label] : JSON.parse(fs.readFileSync(path.join(__dirname, "../../db/data.json"), { encoding: 'utf8', flag: 'r' }))
-}
-
-const writeData = (data, label) => {
-    const _data = readData(null);
-
-    _data[label] = data;
-
-    fs.writeFileSync(path.join(__dirname, "../../db/data.json"), JSON.stringify(_data, null, 2), {
-        encoding: "utf8",
-        flag: "w",
-        mode: 0o666,
-    });
-}
+const { readData, writeData } = require("../../utility/db");
 
 /**
  * @path /api/users
